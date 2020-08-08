f2805b1964cd4aab26e35fa9daad81e1d172e10d
sj <kio8110iop@gmail.com>
2020-08-08 17:48:44 +0900

    test

--- Web/Laravel_Vue/resources/js/components/graph/ChartHeader.vue
+++ Web/Laravel_Vue/resources/js/components/graph/ChartHeader.vue
@@ -4,7 +4,7 @@
       <v-layout justify-center fill-height wrap>
         <v-flex xs12 class="center_card">
           <v-layout row wrap justify-center>
-            <h2 class="orange--text">My nutrition balance</h2>
+            <h2 class="orange--text">栄養バランス</h2>
           </v-layout>
         </v-flex>
         <v-flex xs12 sm12 md12 xl11 class="center_card" style="margin-top:30px;">
